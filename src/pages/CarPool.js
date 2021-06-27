4404ded520c0408fbd7cb0c8c6fbc7da02cb5fe8
Sudha SK <sskumar4@ncsu.edu>
2021-06-27 14:48:06 -0400

    updated text for upcoming events and carpool page

--- src/pages/CarPool.js
+++ src/pages/CarPool.js
@@ -12,11 +12,14 @@ function CarPool(){
             <img src={image} alt="ptaImage" />
             </Box>
              <Box bg="tomato" w="100%" p={4} color="white" marginTop="-150px">
-            <Heading fontSize="20px" textAlign="center">List of Topics</Heading>
+            <Heading fontSize="20px" textAlign="center">Carpool</Heading>
             </Box>
              <Divider orientation="horizontal" />
             <Box bg="gray.500" w="100%" p={4} color="white">
-              
+            <p><strong>Driving your kids every day and have extra seats?</strong></p> You can make it count by inviting others to carpool.
+            Sharing the drives to school, parties, and activities through carpooling will not only lower your stress levels, but it also builds relationships.<br />
+
+            <br />Carpooling is also better for the environment and can help build a better world for our kids to grow up in. By carpooling with 2 other families that’s 2 cars off the road each time your group drives.
             </Box>
            </div>
         );
